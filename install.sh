2c0fdaec0ff223a3d00cea420b5e9840c6e20fb6
maal0lan <gmmaalolan@gmail.com>
2025-09-17 14:45:57 +0530

    new

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt upgrade
+sudo apt install -y tcl ns2 gedit
+git clone https://github.com/maal0lan/ns2_simulation
+cd ns2_simulation
+sudo dpkg -i nam_1.15-10-ubuntu14_amd64.deb
+sudo apt-get install -f
+gedit example.tcl
+
+echo "Installation complete. You can now run your simulation using the command: ns example.tcl"
